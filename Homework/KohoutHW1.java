bcd7dd91394e7de93dcda21e6189ee944c382267
Evan Kohout <ebox8_000@DESKTOP-PC.(none)>
2017-04-12 19:07:03 -0700

    fixed decimal calculation for e^x in HW1

--- Homework/KohoutHW1.java
+++ Homework/KohoutHW1.java
@@ -8,51 +8,44 @@ public class KohoutHW1{
 		System.out.println("=\t===");
 
 		for(int i=-3; i<4; i++){
-			System.out.printf("%d\t%f%n", i, eCalc(i));
+			System.out.println(Integer.toString(i) + "\t" + Double.toString(eCalc(i)));
 		}
-		
 		
 		System.out.println("\nx\tsin(x)");
 		System.out.println("=\t======");
 		
 		for(int i=1; i<=4; i++){
-			System.out.printf("%dPI/2\t%f%n", i, sinCalc((i * Math.PI)/2));
+			System.out.println(Integer.toString(i) + "PI/2" + "\t" + Double.toString(sinCalc((i * Math.PI)/2)));
 		}
-	}
-	
-	public static long factorial(int fact){
-		long factFinal = 1;
-		for(int i=fact; i>0; i--){
-			factFinal *= i;
-		}
-		return factFinal;
 	}
 	
 	public static double eCalc(int xVar){
 		double eToX = 1 + xVar;
-		
+		double factorial = 1;
 		for(int i = 2; i < 15; i++){
-			eToX += (Math.pow(xVar, i)/factorial(i));
+			eToX += (Math.pow(xVar, i)/(factorial *= i));
 		}
-		
 		return eToX;
 	}
 	
 	public static double sinCalc(double xVar){
 		double sinOfX = Math.toRadians(xVar);
-
-		for(int i = 3; i < 17; i += 2){
-			sinOfX -= (Math.pow(xVar, i)/factorial(i));
+		double[] taylorArray = new double[32];
+		double factorial = 1;
+		for(int i = 1; i <= 31; i ++){
+			taylorArray[i] = (Math.pow(sinOfX, i)/(factorial *= i));
+			factorial *= i;
+			
+			//double sign = Math.pow(-1, i);
+			//sinOfX += (Math.pow(xVar, i)/factorial(i));
 			//System.out.println(sinOfX);
 		}
-		
-		for(int i = 5; i < 21; i += 2){
-			sinOfX += (Math.pow(xVar, i)/factorial(i));
-			//System.out.println(sinOfX);
+		for(int i = 3;i < taylorArray.length; i += 4){
+			sinOfX -= taylorArray[i];
 		}
-		
+		for(int i = 5;i < taylorArray.length; i += 4){
+			sinOfX += taylorArray[i];
+		}		
 		return sinOfX;
 	}
-	
-
 }
