8448113a2277b846930683315a7945be8bab3f7a
Kohout <evankoh@amazon.com>
2017-04-28 00:32:21 -0700

    finished implementing logic for Homework3 - SquareRootApp and SquareRootMath classes

--- Homework/SquareRootApp.java
+++ Homework/SquareRootApp.java
@@ -10,7 +10,7 @@ import java.util.Scanner;
 public class SquareRootApp{
     
 	public static void main(String[] args){
-
+		// prints welcome message before menu
 	    System.out.println(" _       __     __\n"
 	    + "| |     / /__  / /________  ____ ___  ___\n"
 	    + "| | /| / / _ \\/ / ___/ __ \\/ __ `__ \\/ _ \\\n"
@@ -19,6 +19,9 @@ public class SquareRootApp{
 		menu();
 	}
 	
+	/**
+	 * Builds menu to be shown to user, 
+	 */
 	public static void menu(){
 		boolean flag = true;
 		Scanner in = new Scanner(System.in);
@@ -31,12 +34,16 @@ public class SquareRootApp{
 		    		+ "2) Approximate area \n"
 		    		+ "3) Quit \n"
 		    		+ "> ");
-	        switch (in.next()) {
-	            case "1":  flag = false;
+		    // switch statement which builds menu structure
+	        switch (in.nextLine()) {
+	            case "1":  
+	            	flag = false;
 	            	findSlope();
-	            case "2":  flag = false;
+	            case "2":  
+	            	flag = false;
 	            	approxArea();
-	            case "3":  System.exit(0);
+	            case "3":  
+	            	System.exit(0);
 	            default: 
 	            	System.out.print("\n!!! Invalid input !!!\n\n");
 	        }
@@ -44,22 +51,48 @@ public class SquareRootApp{
 	    }
 	}
 
+	/**
+	 * method used to make call to slopeOfTangent from SquareRootMath class, prints output
+	 */
 	public static void findSlope(){
-		Scanner in = new Scanner(System.in);
-		System.out.print("Please Enter Number: ");
-		String userInput = in.next();
-		System.out.println(userInput);
+		int xVar = validateInput(0, "Please enter a positive number > 0 to \ncalculate slope of the tangent line: ");
+		System.out.println(SquareRootMath.slopeOfTangent(xVar));
+		System.out.print("\n\n");
 		menu();
 	}
 	
+	/**
+	 * method used to make call to areaUnderGraph from SquareRootMath class, prints output
+	 */
 	public static void approxArea(){
-		Scanner in = new Scanner(System.in);
-		System.out.print("Please Enter Number: ");
-		String userInput = in.next();
-		System.out.println(userInput);
+		int xVar = validateInput(0, "Please enter a positive number > 0 to \n"
+				+ "calculate area under the graph of f(x) = sqrt(x) : \n");
+		System.out.println(SquareRootMath.areaUnderGraph(xVar, 4));
 		menu();
 	}
 	
-	
-	
+	/**
+	 * Validates input as received by user input from menu
+	 * @param x			The value to restrict input by
+	 * @param text		The text to display to the user
+	 * @return
+	 */
+	public static int validateInput(int x, String text){
+		boolean flag = true;
+		int xVar = 0;
+		Scanner in = new Scanner(System.in);
+		while(flag){
+			System.out.print("\n" + text + "\n");
+			if(!in.hasNextInt()){
+				System.out.print("\n\n!!! Invalid input !!!\n");
+				in.nextLine();
+			} else {
+				xVar = in.nextInt();
+				if(xVar > x){
+					flag = false;
+				}	
+			}
+		}
+		return xVar;
+	}
 }
--- Homework/SquareRootMath.java
+++ Homework/SquareRootMath.java
@@ -1,14 +1,29 @@
+import java.lang.Math;
 /**
  * 
  * @author Evan Kohout
  *
  */
 public class SquareRootMath{
-	public static long slopeOfTangent(int x){
-		return x;
+	public static double slopeOfTangent(int x){
+		return 1 / (2 * Math.sqrt(x));
 	}
 	
-	public static long areaUnderGraph(int slope){
-		return slope;
+	public static double areaUnderGraph(int x, int n){
+		// sets 
+		double a = 0.0;
+		double b = x;
+		double width = (b - a) / n; // calculates width of each rectangle
+		double sum = 0.0; // initializes the sum of the rectangles
+		double j;
+		for(int i = 1; i <= n; i++){
+			j = a + (i - 1) * width; 
+			sum += (width * f(j) ); // calculates area
+		}
+		return sum;
+	}
+
+	private static double f(double x){
+		return Math.sqrt(x);
 	}
 }
