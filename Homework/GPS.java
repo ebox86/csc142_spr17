4b8605c71aaac15937bb3415974eecb16cd53b78
Kohout <evankoh@amazon.com>
2017-05-09 00:04:46 -0700

    finished HW4

--- Homework/GPS.java
+++ Homework/GPS.java
@@ -1,17 +1,94 @@
 /**
- * GPS 
+ * GPS class - stores current location and heading
  * 
  * @author Evan Kohout
  *
  */
 
+import java.math.*;
+
 public class GPS {
+
 	
-	
+	Heading heading;
+	CSC142Point currentLoc;
 	
 	// ~~ constructors ~~ //
 	
-	public GPS (Heading currentDir, CSC142Point currentLoc){
+	public GPS (Heading currentDir, CSC142Point currentLoc) throws NullPointerException {
+		if(currentDir == null || currentLoc == null){
+			throw new NullPointerException("Parameters cannot be null!");
+		}
+	}
+	
+	// ~~ methods ~~ //
+	
+	/**
+	 * Move - moves the GPS object the specified amount of units in its current heading
+	 * @param units
+	 * @throws IllegalArgumentException
+	 */
+	public void move(double units) throws IllegalArgumentException {
+		if(units < 0){
+			throw new IllegalArgumentException("Units is less than zero!!!");
+		}
+		
+		// Converting to radians
+		double headingAngleInRadians = this.heading.getHeading() * Math.PI / 180;
+		
+		// obtaining locations from current point
+		double x = this.currentLoc.getX();
+		double y = this.currentLoc.getY();
+		
+		// math for each x and y variable
+		double deltaX = units * Math.sin(headingAngleInRadians);
+		double deltaY = units * Math.cos(headingAngleInRadians);
+		
+		// setting new point
+		currentLoc.setPoint(x + deltaX, y + deltaY);
+	}
+	
+	/**
+	 * Turn method, turns the GPS object the amount of degrees specified
+	 * @param degrees
+	 * @throws IllegalArgumentException
+	 */
+	public void turn(double degrees) throws IllegalArgumentException {
+		if(Math.abs(degrees) > 180){
+			throw new IllegalArgumentException();
+		}
+		
+		double angle = heading.getHeading();
+		angle += degrees;
+		heading.setHeading(angle);
+	}
+	
+	// ~~ accessors ~~ //
+	
+	/**
+	 * getHeading - returns heading
+	 * @return
+	 */
+	public Heading getHeading(){
+		return heading;
+	
+	}
+	
+	/**
+	 * getLocation - returns the location
+	 * @return
+	 */
+	public CSC142Point getLocation(){
+		return currentLoc;
+	}
+	
+	/**
+	 * toString - returns a string representation of the current location and heading
+	 */
+	public String toString(){
+		return "x = " + currentLoc.getX() + ", y = " + currentLoc.getY() + ", heading = " + heading.getHeading();
+	}
+		
 		
 	}
 }
--- Homework/Heading.java
+++ Homework/Heading.java
@@ -1,62 +1,79 @@
 /**
- * Heading class 
+ * Heading class - stores heading
  * 
  * @author Evan Kohout
  *
  */
 
 public class Heading {
-	
-	
-	
+
 	double heading;
-	
+
 	// ~~ constructors ~~ //
-	
+
 	/**
 	 * Heading - default constructor
+	 * 
 	 * @param initialDegrees
 	 */
-	public Heading( double initialDegrees){
-		this.heading = initialDegrees;
+	public Heading(double initialDegrees) throws IllegalArgumentException {
+		setHeading(initialDegrees);
 	}
-	
+
 	// ~~ accessors ~~ //
-	
+
 	/**
 	 * getHeading
+	 * 
 	 * @return
 	 */
-	public double getHeading(){
+	public double getHeading() {
 		return heading;
 	}
-	
-	public String getBearing(){
-		return "north";
+
+	/**
+	 * getBearing - gets current cardinal direction + degree of heading, returns string
+	 * @return
+	 */
+	public String getBearing() {
+		if (heading > 90 && heading < 270) {
+			double degrees = 180 - heading;
+			if (degrees < 0) {
+				return "S " + degrees * -1 + " W";
+			} else {
+				return "S " + degrees + " E";
+			}
+
+		} else {
+			if (heading <= 90) {
+				return "N " + heading + " E";
+			} else {
+				return "N " + (360 - heading) + " W";
+			}
+		}
+
 	}
-	
-	
+
 	// ~~ mutators ~~ //
 
 	/**
+	 * setHeading method - sets the initial heading
 	 * 
 	 * @param update
 	 * @return
 	 */
-	public void setHeading(double update){
+	public void setHeading(double update) throws IllegalArgumentException {
+		if (update < 0 || update > 360) {
+			throw new IllegalArgumentException();
+		}
 		this.heading = update;
 	}
-	
+
 	/**
 	 * toString - output method
 	 */
-	public String toString(){
+	public String toString() {
 		return String.valueOf(heading);
 	}
-	
-	// ~~ test method ~~ //
-	
-	
-	
-	
+
 }
