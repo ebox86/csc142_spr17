60b4a6a8bb345543b5aee8e08c18c320af7fce81
Evan Kohout <ebox8_000@DESKTOP-PC.(none)>
2017-06-06 16:32:45 -0700

    added classes for HW7

--- Homework/MyRectangle.java
+++ Homework/MyRectangle.java
@@ -1,3 +1,8 @@
+/**
+ * MyRectangle -- creates a new MyRectangle object as a child of MyShape
+ * @author evan kohout
+ *
+ */
 public class MyRectangle extends MyShape {
 	public MyRectangle(int h, int w) {
 		super(h, w);
